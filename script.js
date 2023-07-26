a9f019681ad41ca929a15c1b82b4296f7bb750fc
Elmo Andrade <elmosantana07@gmail.com>
2023-07-26 11:56:02 -0300

    Alteração 26/07

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const prova01Button = document.getElementById("prova-01");
     const prova02Button = document.getElementById("prova-02");
+    const prova03Button = document.getElementById("prova-03");
 
     prova01Button.addEventListener("click", function() {
         // Redirecionar para a página da Prova 01 (substitua "prova01.html" pelo nome do arquivo da primeira prova).
@@ -10,5 +11,10 @@ document.addEventListener("DOMContentLoaded", function() {
     prova02Button.addEventListener("click", function() {
         // Redirecionar para a página da Prova 02 (substitua "prova02.html" pelo nome do arquivo da segunda prova).
         window.location.href = "../Simulado/HTML/ProvaN2.html";
+    });
+
+    prova03Button.addEventListener("click", function() {
+        // Redirecionar para a página da Prova 03 (substitua "prova03.html" pelo nome do arquivo da segunda prova).
+        window.location.href = "../Simulado/HTML/ProvaN3.html";
     });
 });
